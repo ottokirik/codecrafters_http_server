609cc9ab1852a3041301b0356f463551f77c4ce9
Petrov Kirill <ottokirik@gmail.com>
2024-07-09 16:50:40 +0300

    add spawn thread

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::{
     io::{Read, Write},
     net::TcpListener,
+    thread::spawn,
 };
 
 fn main() {
@@ -17,41 +18,43 @@ fn main() {
             Ok(mut stream) => {
                 println!("accepted new connection");
 
-                let mut buffer = [0; 1024];
-                let bytes_read = stream.read(&mut buffer).expect("");
+                let thr = spawn(move || {
+                    let mut buffer = [0; 1024];
+                    let bytes_read = stream.read(&mut buffer).expect("");
 
-                let request_string = String::from_utf8_lossy(&buffer[0..bytes_read]);
-                let request_vec = request_string.split("\r\n").collect::<Vec<_>>();
+                    let request_string = String::from_utf8_lossy(&buffer[0..bytes_read]);
+                    let request_vec = request_string.split("\r\n").collect::<Vec<_>>();
 
-                let request_line = request_vec
-                    .iter()
-                    .find(|str| str.starts_with("GET"))
-                    .unwrap();
+                    let request_line = request_vec
+                        .iter()
+                        .find(|str| str.starts_with("GET"))
+                        .unwrap();
 
-                let requested_resource = request_line.split(" ").nth(1).unwrap();
+                    let requested_resource = request_line.split(" ").nth(1).unwrap();
 
-                let response = match requested_resource {
-                    "/" => "HTTP/1.1 200 OK\r\n\r\n".to_owned(),
+                    let response = match requested_resource {
+                        "/" => "HTTP/1.1 200 OK\r\n\r\n".to_owned(),
 
-                    s if s.starts_with("/echo/") => {
-                        let word = s.split("/").nth(2).unwrap();
-                        format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", word.len(), word)
-                    }
+                        s if s.starts_with("/echo/") => {
+                            let word = s.split("/").nth(2).unwrap();
+                            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", word.len(), word)
+                        }
 
-                    s if s.starts_with("/user-agent") => {
-                        let user_agent_line = request_vec
-                            .iter()
-                            .find(|str| str.starts_with("User-Agent:"))
-                            .unwrap_or(&"User-Agent: Unknown");
-                        let user_agent = user_agent_line.split(" ").nth(1).unwrap();
+                        s if s.starts_with("/user-agent") => {
+                            let user_agent_line = request_vec
+                                .iter()
+                                .find(|str| str.starts_with("User-Agent:"))
+                                .unwrap_or(&"User-Agent: Unknown");
+                            let user_agent = user_agent_line.split(" ").nth(1).unwrap();
 
-                        format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent)
-                    }
+                            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent)
+                        }
 
-                    _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_owned(),
-                };
+                        _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_owned(),
+                    };
 
-                stream.write_all(response.as_bytes()).unwrap();
+                    stream.write_all(response.as_bytes()).unwrap();
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
