f7457d96cd0ef40d3f50d23f13ba89882de4a765
Petrov Kirill <ottokirik@gmail.com>
2024-07-09 15:20:26 +0300

    parse request line

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,9 +16,30 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream
-                    .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
-                    .expect("200 \n");
+
+                let mut buffer = [0; 1024];
+                let bytes_read = stream.read(&mut buffer).expect("");
+
+                let request_string = String::from_utf8_lossy(&buffer[0..bytes_read]);
+
+                let request_line = request_string
+                    .split("\r\n")
+                    .find(|str| str.starts_with("GET"))
+                    .unwrap();
+
+                let requested_resource = request_line.split(" ").nth(1).unwrap();
+
+                println!("request: {:?}", requested_resource);
+
+                if requested_resource == "/" {
+                    stream
+                        .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                        .expect("200 \n");
+                } else {
+                    stream
+                        .write_all(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                        .expect("404 \n");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
