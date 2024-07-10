a553bea6c2542893ea264221670fdd055068119d
Petrov Kirill <ottokirik@gmail.com>
2024-07-10 14:51:06 +0300

    gzip encoding

--- src/main.rs
+++ src/main.rs
@@ -42,24 +42,31 @@ fn handle_connect(mut stream: TcpStream) {
         .find(|str| str.starts_with("Accept-Encoding:"))
         .unwrap_or(&"");
 
-    let is_gzip = encoding.contains("gzip");
+    let encoding = match encoding.contains("gzip") {
+        true => "gzip",
+        false => "",
+    };
 
     let request_line = request_vec.first().unwrap();
 
     let requested_resource = request_line.split_whitespace().nth(1).unwrap();
 
     let response = match request_line {
-        s if s.starts_with("GET / ") => {
-            "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\n\r\n".to_owned()
-        }
+        s if s.starts_with("GET / ") => HTTPRequestBuilder::default()
+            .add_encoding(encoding)
+            .build()
+            .into(),
 
         s if s.starts_with("GET /echo/") => {
             let word = requested_resource.split("/").nth(2).unwrap();
-            format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\nContent-Encoding: gzip\r\n\r\n{}",
-                word.len(),
-                word
-            )
+
+            HTTPRequestBuilder::default()
+                .add_encoding(encoding)
+                .add_content_type("text/plain")
+                .add_content_length(word.len())
+                .add_content(word)
+                .build()
+                .into()
         }
 
         s if s.starts_with("GET /user-agent") => {
@@ -69,11 +76,13 @@ fn handle_connect(mut stream: TcpStream) {
                 .unwrap_or(&"User-Agent: Unknown");
             let user_agent = user_agent_line.split_whitespace().nth(1).unwrap();
 
-            format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\nContent-Encoding: gzip\r\n\r\n{}",
-                user_agent.len(),
-                user_agent
-            )
+            HTTPRequestBuilder::default()
+                .add_encoding(encoding)
+                .add_content_type("text/plain")
+                .add_content_length(user_agent.len())
+                .add_content(user_agent)
+                .build()
+                .into()
         }
 
         s if s.starts_with("GET /files") => {
@@ -82,13 +91,20 @@ fn handle_connect(mut stream: TcpStream) {
             let dir = env_args.get(2).unwrap();
             let path = format!("{}{}", dir, file_name);
 
-            let request = match fs::read_to_string(path) {
-                Ok(content) => format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\nContent-Encoding: gzip\r\n\r\n{}",
-                    content.len(),
-                    content
-                ),
-                Err(_) => "HTTP/1.1 404 Not Found\r\nContent-Encoding: gzip\r\n\r\n".to_owned()
+            let request: String = match fs::read_to_string(path) {
+                Ok(content) => HTTPRequestBuilder::default()
+                    .add_encoding(encoding)
+                    .add_content_type("application/octet-stream")
+                    .add_content_length(content.len())
+                    .add_content(content.as_str())
+                    .build()
+                    .into(),
+
+                Err(_) => HTTPRequestBuilder::default()
+                    .add_status("404 Not Found")
+                    .add_encoding(encoding)
+                    .build()
+                    .into(),
             };
 
             request
@@ -103,16 +119,87 @@ fn handle_connect(mut stream: TcpStream) {
 
             file.write_all(content.as_bytes()).unwrap();
 
-            format!("HTTP/1.1 201 Created\r\nContent-Encoding: gzip\r\n\r\n")
+            HTTPRequestBuilder::default()
+                .add_status("201 Created")
+                .add_encoding(encoding)
+                .build()
+                .into()
         }
 
-        _ => "HTTP/1.1 404 Not Found\r\nContent-Encoding: gzip\r\n\r\n".to_owned(),
-    };
-
-    let response = match is_gzip {
-        true => response,
-        false => response.replace("Content-Encoding: gzip\r\n", ""),
+        _ => HTTPRequestBuilder::default()
+            .add_status("404 Not Found")
+            .add_encoding(encoding)
+            .build()
+            .into(),
     };
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+#[derive(Debug)]
+struct HTTPRequest(String);
+
+impl From<HTTPRequest> for String {
+    fn from(request: HTTPRequest) -> Self {
+        request.0
+    }
+}
+
+struct HTTPRequestBuilder {
+    protocol: String,
+    status: String,
+    content_length: String,
+    content_type: String,
+    content_encoding: String,
+    content: String,
+}
+
+impl HTTPRequestBuilder {
+    fn default() -> Self {
+        Self {
+            protocol: "HTTP/1.1".to_owned(),
+            status: "200 OK\r\n".to_owned(),
+            content_length: "".to_owned(),
+            content_type: "".to_owned(),
+            content_encoding: "".to_owned(),
+            content: "".to_owned(),
+        }
+    }
+
+    fn build(&self) -> HTTPRequest {
+        HTTPRequest(format!(
+            "{} {}{}{}{}\r\n{}",
+            self.protocol,
+            self.status,
+            self.content_type,
+            self.content_encoding,
+            self.content_length,
+            self.content
+        ))
+    }
+
+    fn add_encoding(mut self, encoding: &str) -> Self {
+        self.content_encoding = format!("Content-Encoding: {}\r\n", encoding);
+        self
+    }
+
+    fn add_content_type(mut self, content_type: &str) -> Self {
+        self.content_type = format!("Content-Type: {}\r\n", content_type);
+        self
+    }
+
+    fn add_content_length(mut self, content_length: usize) -> Self {
+        self.content_length = format!("Content-Length: {}\r\n", content_length);
+        self
+    }
+
+    fn add_content(mut self, content: &str) -> Self {
+        self.content = content.to_owned();
+        self
+    }
+
+    fn add_status(mut self, status: &str) -> Self {
+        self.status = format!("{}\r\n", status);
+        self
+    }
 }
